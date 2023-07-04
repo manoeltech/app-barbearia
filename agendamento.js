edb2c879af6776675ad6c366d3a47fac753981f5
manoeltech <manoel.programador21@gmail.com>
2023-07-04 20:03:48 -0300

    Improve agendamento.js file

---
+++ agendamento.js
@@ -0,0 +1,7 @@
+const selectHour = document.querySelector('#selectHour')
+const btnSubmit2 = document.querySelector('#btnSubmit2')
+const textResult2 = document.querySelector('#textResult2')
+
+btnSubmit2.addEventListener('click', function(){
+  window.location.href = 'confirmacao.html'
+})
