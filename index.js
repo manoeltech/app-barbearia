884d90d6a3e8b27d8f141bd672b35b007aa9767c
manoeltech <manoel.programador21@gmail.com>
2023-07-04 19:05:47 -0300

    Create form in javascript for screen index.html

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const getService = document.querySelector('#selectService')
+const btnSubmit = document.querySelector('#btnSubmit')
+const textResult = document.querySelector('#textResult')
+
+getService.addEventListener('click', function(){
+  let price = getService.value 
+  textResult.innerHTML = `Total: R$ ${price}`
+})
+
+btnSubmit.addEventListener('click', function(){
+  window.location.href = 'agendamento.html'
+})
+
