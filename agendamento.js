858a55625ccfc4c23cbf4ea45c5cd338fc968d9e
manoeltech <manoel.programador21@gmail.com>
2023-07-04 20:22:28 -0300

    Improve button back

--- agendamento.js
+++ agendamento.js
@@ -5,3 +5,8 @@ const textResult2 = document.querySelector('#textResult2')
 btnSubmit2.addEventListener('click', function(){
   window.location.href = 'confirmacao.html'
 })
+
+btnBack.addEventListener('click', function(){
+  window.location.href = 'index.html'
+
+})
